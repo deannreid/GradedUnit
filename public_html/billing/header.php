e6d029c0e54ee5ebe43cc5151bafa17447692762
Dean Reid <deannreid@hotmail.co.uk>
2019-05-10 20:02:10 +0100

    Some Code Cleanup
    Some Code Cleanup

--- public_html/billing/header.php
+++ public_html/billing/header.php
@@ -1,111 +1,100 @@
 <?php
 // we generate the navbar components in case they weren't before
-if($page->navbar == array())
+if ($page->navbar == array()) {
     $page->navbar = $presets->GenerateNavbar();
+}
 
-if(!$user->islg()) // if it's not logged in we hide the user menu
-    unset($page->navbar[count($page->navbar)-1]);
+if (!$user->islg()) { // if it's not logged in we hide the user menu
+    unset($page->navbar[count($page->navbar) - 1]);
+}
 ?>
 
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <title>XenoHost - Fast | Reliable | Affordable</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap -->
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-        <!--==============GOOGLE FONT - OPEN SANS=================-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<head>
+    <meta charset="utf-8">
+    <title>XenoHost - Fast | Reliable | Affordable</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="../assets/css/hosting.css" rel="stylesheet" media="all">
+    <script src="../assets/js/modernizr.js"></script>
+</head>
 
-        <link href="../assets/css/hosting.css" rel="stylesheet" media="all">
 
-        <!--==============Modernizr =================-->
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation"> 
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                <a class="navbar-brand" href="index.php"> <img src="../assets/images/logo.png" alt="logo"></a> 
+            </div>          
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+<?php
+    foreach ($page->navbar as $key => $v) {
+                        //Single Button Item
+        if ($v[0] == 'item') {
+            echo "<li" . ($v[1]['class'] ? " class='" . $v[1]['class'] . "'" : "") . ">
+                                                                <a href='" . $v[1]['href'] . "'>" . $v[1]['name'] . "</a></li>";
+        } else if ($v[0] == 'dropdown') {
+            echo "<li class='dropdown".
+            ($v['class'] ? " " . $v['class'] : "") . "'" . ">
+            <a href='#' class='dropdown' data-toggle='dropdown'>" . $v['name'] . " <b class='caret'></b></a>
+            <ul class='dropdown-menu'>";
+            foreach ($v[1] as $k => $v)
+                echo "<li" .
+                ($v['class'] ? " class='" . $v['class'] . "'" : "") . ">
 
-        <script src="../assets/js/modernizr.js"></script>
-    </head>
-    
-	
-	<body>
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation"> 
-            <!-- Brand and toggle get grouped for better mobile display -->
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-					<a class="navbar-brand" href="index.php"> <img src="../assets/images/logo.png" alt="logo"></a> 
-				</div>          
-				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<ul class="nav navbar-nav navbar-right">
-						<?php
-							foreach ($page->navbar as $key => $v) {
-								//Single Button Item
-								if ($v[0] == 'item') {
-									echo "<li".($v[1]['class'] ? " class='".$v[1]['class']."'" : "").">
-										<a href='".$v[1]['href']."'>".$v[1]['name']."</a></li>";
-								
-								
-								} else if($v[0] == 'dropdown') {
-									echo "<li class='dropdown".
-										// extra classes 
-										($v['class'] ? " ".$v['class'] : "")."'".">
-										<a href='#' class='dropdown' data-toggle='dropdown'>".$v['name']." <b class='caret'></b></a>
-										<ul class='dropdown-menu'>";
-									foreach ($v[1] as $k => $v) 
-										echo "<li".
-											
-											($v['class'] ? " class='".$v['class']."'" : "").">
+                                                                        <a href='" . $v['href'] . "'>" . $v['name'] . "</a></li>
+                                                                ";
 
-											<a href='".$v['href']."'>".$v['name']."</a></li>
-										";                                
-						
-						echo"	</li> 
-							</ul>
-						";
-								}
-								
-							}
-							if(!$user->islg()) {
-							echo "
+            echo"	</li> 
+                                        </ul>
+                                ";
+        }
+    }
+                    if (!$user->islg()) {
+                        echo "
 									<a class=\"btn btn-large\" href=\"$set->url/login.php\">Login</a>
 									<a class=\"btn btn-success\" href=\"$set->url/register.php\">Sign Up</a>
 
 							";
-							}
+                    }
 
-echo "			</div>
+                    echo "			</div>
 			</div>
 		</nav>";
-		
-     
-		
-		if($user->data->banned) {
-  
-    // we delete the expired banned
-    $_unban = $db->getAll("SELECT `userid` FROM `".MLS_PREFIX."banned` WHERE `until` < ".time());
-    if($_unban) 
-        foreach ($_unban as $_usr) {
-            $db->query("DELETE FROM `".MLS_PREFIX."banned` WHERE `userid` = ?i", $_usr->userid);
-            $db->query("UPDATE `".MLS_PREFIX."users` SET `banned` = '0' WHERE `userid` = ?i", $_usr->userid);             
-        }
 
 
-    $_banned = $user->getBan();
-    if($_banned)
-    $options->error("You were banned by <a href='$set->url/profile.php?u=$_banned->by'>".$user->showName($_banned->by)."</a> for `<i>".$options->html($_banned->reason)."</i>`.
-        Your ban will expire in ".$options->tsince($_banned->until, "from now.")."
+
+                    if ($user->data->banned) {
+
+                        // we delete the expired banned
+                        $_unban = $db->getAll("SELECT `userid` FROM `" . MLS_PREFIX . "banned` WHERE `until` < " . time());
+                        if ($_unban)
+                            foreach ($_unban as $_usr) {
+                                $db->query("DELETE FROM `" . MLS_PREFIX . "banned` WHERE `userid` = ?i", $_usr->userid);
+                                $db->query("UPDATE `" . MLS_PREFIX . "users` SET `banned` = '0' WHERE `userid` = ?i", $_usr->userid);
+                            }
+
+
+                        $_banned = $user->getBan();
+                        if ($_banned)
+                            $options->error("You were banned by <a href='$set->url/profile.php?u=$_banned->by'>" . $user->showName($_banned->by) . "</a> for `<i>" . $options->html($_banned->reason) . "</i>`.
+        Your ban will expire in " . $options->tsince($_banned->until, "from now.") . "
         ");
-}
+                    }
 
-if($user->islg() && $set->email_validation && ($user->data->validated != 1)) {
-    $options->fError("Your account is not yet acctivated ! Please check your email !");
-}
+                    if ($user->islg() && $set->email_validation && ($user->data->validated != 1)) {
+                        $options->fError("Your account is not yet acctivated ! Please check your email !");
+                    }
 
-if(isset($_SESSION['success'])){
-    $options->success($_SESSION['success']);
-    unset($_SESSION['success']);
-}
-if(isset($_SESSION['error'])){
-    $options->error($_SESSION['error']);
-    unset($_SESSION['error']);
-
-}
-flush(); // we flush the content so the browser can start the download of css/js
+                    if (isset($_SESSION['success'])) {
+                        $options->success($_SESSION['success']);
+                        unset($_SESSION['success']);
+                    }
+                    if (isset($_SESSION['error'])) {
+                        $options->error($_SESSION['error']);
+                        unset($_SESSION['error']);
+                    }
+                    flush(); // we flush the content so the browser can start the download of css/js
--- public_html/billing/index.php
+++ public_html/billing/index.php
@@ -6,102 +6,104 @@ $page->title = $set->site_name;
 
 $presets->setActive("home"); // we highlight the home link
 
-
 include 'header.php';
 
 
-//Insert the Main Page Here
-echo "
-		
-	        <!--==============HEADER =================-->
-        <div class='jumbotron masthead'>
-            <div class='container'> 
-
-                <!--==============Hero Unit=================-->
-                        <li>
-                            <div class='hero-unit'>
-                                <h1>XenoHost</h1>
-                                <h3>Fast | Reliable | Affordable</h3>
-                                <h4>High Performance hosting at affordable prices</h4>
-                                <div class='slider-actions text-center'> <a href='features.php' class='btn btn-success btn-lg'>Explore Features </a> <a href='pricing.html' class='btn btn-primary btn-lg'>Plans and Pricing </a> </div>
-                            </div>
-                        </li>
-            </div>
-        </div>
-		
+//Banner Unit
+echo"
+    <div class='jumbotron masthead'>
         <div class='container'> 
-            <!--============== Main Features ==============-->
-
-            <div class='row mainFeatures' id='features'>
-                <div class='col-sm-6 col-md-4'>
-                    <div class='img-thumbnail'> <img src='../assets/images/secure_img.png' width='85' height='88' alt='secure'>
-                        <div class='caption'>
-                            <h4>Secure & Reliable</h4>
-                            <p>All XenoHost services have high physical security and power redundancy so your data will always be secure.</p>
-                        </div>
-                    </div>
+            <li>
+                <div class='hero-unit'>
+                    <h1>XenoHost</h1>
+                    <h3>Fast | Reliable | Affordable</h3>
+                    <h4>High Performance hosting at affordable prices</h4>
+                    <div class='slider-actions text-center'> <a href='features.php' class='btn btn-success btn-lg'>Explore Features </a> <a href='pricing.html' class='btn btn-primary btn-lg'>Plans and Pricing </a> </div>
                 </div>
-                <div class='col-sm-6 col-md-4'>
-                    <div class='img-thumbnail'> <img src='../assets/images/fast_img.png' width='85' height='88' alt='secure'>
-                        <div class='caption'>
-                            <h4>Anti-DDoS</h4>
-                            <p>All our services come default with the best anti-DDoS solution on the market and provides round-the-clock protection against many types of attacks without limitations.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class='col-sm-6 col-md-4 col-sm-offset-3 col-md-offset-0'>
-                    <div class='img-thumbnail'> <img src='../assets/images/support_img.png' width='85' height='88' alt='secure'>
-                        <div class='caption'>
-                            <h4>Customer Support</h4>
-                            <p>We have a dedicated team of support for sales and support to help you in anytime. You can also chat with us using out Live-Chat software.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!--============== Other Features ==============-->
-
-            <div class='row PageHead'>
-                <div class='col-md-12'>
-                    <h1>More Features</h1>
-                    <h3>You will get lot of features with flathost</h3>
-                </div>
-            </div>
-            <div class='row'>
-                <div class='col-sm-6 features'> <img src='../assets/images/setup_icon.png' alt='icon' class='img-responsive'>
-                    <h4>Instant Setup</h4>
-                    <p>As soon as you make a successful payment via PayPal or Google Checkout, your web hosting and domain names will be activated immediately. No waiting time whatsoever.</p>
-                </div>
-                <div class='col-sm-6 features'> <img src='../assets/images/backup_icon.png' alt='icon' class='img-responsive'>
-                    <h4>Constant Backups</h4>
-                    <p>Your hosting account is backed up 4 times a day as standard, with our backup integration. We use dedicated backup servers, providing fast & easy individual file rollback abilities.</p>
-                </div>
-            </div>
-            <div class='row'>
-                <div class='col-sm-6 features'> <img src='../assets/images/git_icon.png' alt='icon' class='img-responsive'>
-                    <h4>GIT/SVN Support</h4>
-                    <p>Web Developers love using version control systems. All of our hosting accounts can use GIT & SVN command line tools on our servers. Simply request SSH access to get started.</p>
-                </div>
-                <div class='col-sm-6 features'> <img src='../assets/images/script_icon.png' alt='icon' class='img-responsive'>
-                    <h4>280+ Install Scripts</h4>
-                    <p>All our hosting accounts allow you to install popular software such as Wordpress, Drupal, Joolma and Magento in one easy step. Upgrading your software is just as easy!</p>
-                </div>
-            </div>
-            <div class='row'>
-                <div class='col-sm-6 features'> <img src='../assets/images/cpanel_icon.png' alt='icon' class='img-responsive'>
-                    <h4>cPanel Included</h4>
-                    <p>All hosting accounts come with the latest version of cPanel. This makes life easy for you to do routine tasks such as setting up email addresses and managing MySQL databases.</p>
-                </div>
-                <div class='col-sm-6 features'> <img src='../assets/images/php_icon.png' alt='icon' class='img-responsive'>
-                    <h4>Latest PHP &amp; MySQL</h4>
-                    <p>Our network runs the latest stable and secure versions of PHP & MySQL, You also have the option to select any version of PHP you want. We also implement strict security and firewall rules protecting your website from unwanted visitors 24/7.</p>
-                </div>
-            </div>
+            </li>
         </div>
-        <!--Container Closed--> 
+    </div>
 ";
 
+//Main Features
+echo "
+    <div class='container'> 
+        <div class='row mainFeatures' id='features'>
+            <div class='col-sm-6 col-md-4'>
+                <div class='img-thumbnail'> <img src='../assets/images/secure_img.png' width='85' height='88' alt='secure'>
+                    <div class='caption'>
+                        <h4>Secure & Reliable</h4>
+                        <p>All XenoHost services have high physical security and power redundancy so your data will always be secure.</p>
+                    </div>
+                </div>
+            </div>
 
+            <div class='col-sm-6 col-md-4'>
+                <div class='img-thumbnail'> <img src='../assets/images/fast_img.png' width='85' height='88' alt='secure'>
+                    <div class='caption'>
+                        <h4>Anti-DDoS</h4>
+                        <p>All our services come default with the best anti-DDoS solution on the market and provides round-the-clock protection against many types of attacks without limitations.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class='col-sm-6 col-md-4 col-sm-offset-3 col-md-offset-0'>
+                <div class='img-thumbnail'> <img src='../assets/images/support_img.png' width='85' height='88' alt='secure'>
+                    <div class='caption'>
+                        <h4>Customer Support</h4>
+                        <p>We have a dedicated team of support for sales and support to help you in anytime. You can also chat with us using out Live-Chat software.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+";
+//Extra Features
+echo"
+        <div class='row PageHead'>
+            <div class='col-md-12'>
+                <h1>More Features</h1>
+                <h3>You will get lot of features with XenoHost</h3>
+            </div>
+        </div>
+        
+        <div class='row'>
+            <div class='col-sm-6 features'> 
+                <img src='../assets/images/setup_icon.png' alt='icon' class='img-responsive'>
+                <h4>Instant Setup</h4>
+                <p>As soon as you make a successful payment via PayPal or Google Checkout, your web hosting and domain names will be activated immediately. No waiting time whatsoever.</p>
+            </div>
+            <div class='col-sm-6 features'> 
+                <img src='../assets/images/backup_icon.png' alt='icon' class='img-responsive'>
+                <h4>Constant Backups</h4>
+                <p>Your hosting account is backed up 4 times a day as standard, with our backup integration. We use dedicated backup servers, providing fast & easy individual file rollback abilities.</p>
+            </div>
+        </div>
 
+        <div class='row'>
+            <div class='col-sm-6 features'> 
+                <img src='../assets/images/git_icon.png' alt='icon' class='img-responsive'>
+                <h4>GIT/SVN Support</h4>
+                <p>Web Developers love using version control systems. All of our hosting accounts can use GIT & SVN command line tools on our servers. Simply request SSH access to get started.</p>
+            </div>
+            <div class='col-sm-6 features'> 
+                <img src='../assets/images/script_icon.png' alt='icon' class='img-responsive'>
+                <h4>280+ Install Scripts</h4>
+                <p>All our hosting accounts allow you to install popular software such as Wordpress, Drupal, Joolma and Magento in one easy step. Upgrading your software is just as easy!</p>
+            </div>
+        </div>
 
+        <div class='row'>
+            <div class='col-sm-6 features'> 
+                <img src='../assets/images/cpanel_icon.png' alt='icon' class='img-responsive'>
+                <h4>cPanel Included</h4>
+                <p>All hosting accounts come with the latest version of cPanel. This makes life easy for you to do routine tasks such as setting up email addresses and managing MySQL databases.</p>
+            </div>
+            <div class='col-sm-6 features'> 
+                <img src='../assets/images/php_icon.png' alt='icon' class='img-responsive'>
+                <h4>Latest PHP &amp; MySQL</h4>
+                <p>Our network runs the latest stable and secure versions of PHP & MySQL, You also have the option to select any version of PHP you want. We also implement strict security and firewall rules protecting your website from unwanted visitors 24/7.</p>
+            </div>
+        </div>
+    </div>
+";
 include 'footer.php';
